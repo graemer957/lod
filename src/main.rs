e4ca5becbd806132da189055aa1985838544ba83
Graeme Read <graeme@sigma957.net>
2023-10-24 20:08:39 +0100

    Basic loop waiting for commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use lod::say_hello;
+use std::io;
 
-fn main() {
-    say_hello()
+fn main() -> Result<(), io::Error> {
+    let stdin = io::stdin();
+
+    loop {
+        let mut command = String::new();
+        stdin.read_line(&mut command)?;
+
+        match command[..].trim_end() {
+            "quit" => break,
+            "dock" => println!("you want to do something with the dock?"),
+            _ => println!("Unknown command: {command}"),
+        }
+    }
+
+    Ok(())
 }

80ddaaacb5cb2bedc788f9e22c07097e1badd144
Graeme Read <graeme@sigma957.net>
2023-10-27 20:32:25 +0100

    Add functionality to determine if the dock is set to autohide

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
+use std::error::Error;
+use std::{io, process::Command};
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
 
     loop {
@@ -9,10 +10,46 @@ fn main() -> Result<(), io::Error> {
 
         match command[..].trim_end() {
             "quit" => break,
-            "dock" => println!("you want to do something with the dock?"),
+            "dock" => {
+                if dock_autohide()? {
+                    println!("dock is set to autohide");
+                } else {
+                    println!("dock is NOT set to autohide");
+                }
+            }
             _ => println!("Unknown command: {command}"),
         }
     }
 
     Ok(())
+}
+
+const NUMBER_ZERO_IN_ASCII: u8 = 48;
+const NUMBER_ONE_IN_ASCII: u8 = 49;
+
+/// Make use of the `defaults` builtin macOS command line tool to get if the Dock is set to autohide
+///
+/// **NOTE**: I may look for a better way™ to do this in future - this is a quick hack project after all ;-)
+fn dock_autohide() -> Result<bool, Box<dyn Error>> {
+    let output = Command::new("defaults")
+        .args(["read", "com.apple.dock", "autohide"])
+        .output()?;
+
+    let status_code = output.status.code().ok_or("Could not get status code")?;
+    if status_code != 0 {
+        return Err("Got non-zero exit status for `defaults`".into());
+    }
+    if output.stdout.len() != 2 {
+        return Err("Got more chars from output than expected".into());
+    }
+    let digit = output
+        .stdout
+        .first()
+        .ok_or("Could not get first byte of output")?;
+
+    match *digit {
+        NUMBER_ZERO_IN_ASCII => Ok(false),
+        NUMBER_ONE_IN_ASCII => Ok(true),
+        _ => panic!("unexpected {digit}"),
+    }
 }

7ad133851ecae8ee6b9d5575f58dd5d22e013e97
Graeme Read <graeme@sigma957.net>
2023-10-27 20:34:27 +0100

    Relocate logic into library

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
-pub fn say_hello() {
-    println!("Hello, world!")
+use std::error::Error;
+use std::process::Command;
+
+const NUMBER_ZERO_IN_ASCII: u8 = 48;
+const NUMBER_ONE_IN_ASCII: u8 = 49;
+
+/// Make use of the `defaults` builtin macOS command line tool to get if the Dock is set to autohide
+///
+/// **NOTE**: I may look for a better way™ to do this in future - this is a quick hack project after all ;-)
+pub fn dock_autohide() -> Result<bool, Box<dyn Error>> {
+    let output = Command::new("defaults")
+        .args(["read", "com.apple.dock", "autohide"])
+        .output()?;
+
+    let status_code = output.status.code().ok_or("Could not get status code")?;
+    if status_code != 0 {
+        return Err("Got non-zero exit status for `defaults`".into());
+    }
+    if output.stdout.len() != 2 {
+        return Err("Got more chars from output than expected".into());
+    }
+    let digit = output
+        .stdout
+        .first()
+        .ok_or("Could not get first byte of output")?;
+
+    match *digit {
+        NUMBER_ZERO_IN_ASCII => Ok(false),
+        NUMBER_ONE_IN_ASCII => Ok(true),
+        _ => panic!("unexpected {digit}"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{io, process::Command};
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         match command[..].trim_end() {
             "quit" => break,
             "dock" => {
-                if dock_autohide()? {
+                if lod::dock_autohide()? {
                     println!("dock is set to autohide");
                 } else {
                     println!("dock is NOT set to autohide");
@@ -22,34 +22,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-const NUMBER_ZERO_IN_ASCII: u8 = 48;
-const NUMBER_ONE_IN_ASCII: u8 = 49;
-
-/// Make use of the `defaults` builtin macOS command line tool to get if the Dock is set to autohide
-///
-/// **NOTE**: I may look for a better way™ to do this in future - this is a quick hack project after all ;-)
-fn dock_autohide() -> Result<bool, Box<dyn Error>> {
-    let output = Command::new("defaults")
-        .args(["read", "com.apple.dock", "autohide"])
-        .output()?;
-
-    let status_code = output.status.code().ok_or("Could not get status code")?;
-    if status_code != 0 {
-        return Err("Got non-zero exit status for `defaults`".into());
-    }
-    if output.stdout.len() != 2 {
-        return Err("Got more chars from output than expected".into());
-    }
-    let digit = output
-        .stdout
-        .first()
-        .ok_or("Could not get first byte of output")?;
-
-    match *digit {
-        NUMBER_ZERO_IN_ASCII => Ok(false),
-        NUMBER_ONE_IN_ASCII => Ok(true),
-        _ => panic!("unexpected {digit}"),
-    }
 }

e21f61524fcf483f331e28e566f3e15d372909a6
Graeme Read <graeme@sigma957.net>
2023-11-12 07:48:00 +0000

    Imnplement wrapper around `Command`
    - Handles non-zero status codes
    - Supports `Display` and `Debug`

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@
 mod app_state;
 #[cfg(target_os = "macos")]
 pub use app_state::{AppState, Mode};
+mod command;
 #[cfg(target_os = "macos")]
 mod menu_item;
 
+use command::Command;
 use std::error::Error;
-use std::process::Command;
 
 const NUMBER_ZERO_IN_ASCII: u8 = 48;
 const NUMBER_ONE_IN_ASCII: u8 = 49;
@@ -15,25 +16,19 @@ const NUMBER_ONE_IN_ASCII: u8 = 49;
 ///
 /// **NOTE**: I may look for a better way™ to do this in future - this is a quick hack project after all ;-)
 pub fn dock_autohide() -> Result<bool, Box<dyn Error>> {
-    let output = Command::new("defaults")
-        .args(["read", "com.apple.dock", "autohide"])
-        .output()?;
+    let result = Command::new("defaults", &["read", "com.apple.dock", "autohide"]).execute()?;
 
-    let status_code = output.status.code().ok_or("Could not get status code")?;
-    if status_code != 0 {
-        return Err("Got non-zero exit status for `defaults`".into());
-    }
-    if output.stdout.len() != 2 {
+    if result.stdout().len() != 2 {
         return Err("Got more chars from output than expected".into());
     }
-    let digit = output
-        .stdout
+    let digit = result
+        .stdout()
         .first()
         .ok_or("Could not get first byte of output")?;
 
     match *digit {
         NUMBER_ZERO_IN_ASCII => Ok(false),
         NUMBER_ONE_IN_ASCII => Ok(true),
-        _ => panic!("unexpected {digit}"),
+        _ => panic!("Unexpected {digit}"),
     }
 }

e767c29da7760b910ab24faf4ee9230652705a97
Graeme Read <graeme@sigma957.net>
2023-11-26 06:10:13 +0000

    Add methods to Mode for SF Symbol and Accessibility Description

--- src/app_state.rs
+++ src/app_state.rs
@@ -9,8 +9,35 @@ pub enum Mode {
     Desktop,
 }
 
-const LAPTOP_CHAR: &str = "💻";
-const DESKTOP_CHAR: &str = "🖥";
+impl Mode {
+    const fn accessibility_description(&self) -> &'static str {
+        match self {
+            Self::Laptop => "Switch to Laptop mode",
+            Self::Desktop => "Switch to Desktop mode",
+        }
+    }
+
+    const fn description(&self) -> &'static str {
+        match self {
+            Self::Laptop => "Laptop Mode",
+            Self::Desktop => "Desktop Mode",
+        }
+    }
+
+    const fn sf_symbol(&self) -> &'static str {
+        match self {
+            Self::Laptop => "laptopcomputer",
+            Self::Desktop => "desktopcomputer",
+        }
+    }
+
+    const fn toggle(&self) -> Self {
+        match self {
+            Self::Laptop => Self::Desktop,
+            Self::Desktop => Self::Laptop,
+        }
+    }
+}
 
 pub struct AppState {
     status_item: Rc<RefCell<StatusItem>>,
@@ -23,7 +50,10 @@ impl AppState {
     #[must_use]
     pub fn new(mode: Mode) -> Self {
         let mut status_item = StatusItem::new("", Menu::new(vec![]));
-        status_item.set_image_with_system_symbol_name("laptopcomputer", Some(""));
+        status_item.set_image_with_system_symbol_name(
+            mode.sf_symbol(),
+            Some(mode.accessibility_description()),
+        );
 
         Self {
             status_item: Rc::new(RefCell::new(status_item)),
@@ -43,31 +73,27 @@ impl AppState {
     ///
     /// If unable to `borrow_mut` on self(!)
     pub fn toggle_mode(&mut self) {
-        let mode = match self.mode {
-            Mode::Laptop => {
-                self.mode = Mode::Desktop;
-                DESKTOP_CHAR
-            }
-            Mode::Desktop => {
-                self.mode = Mode::Laptop;
-                LAPTOP_CHAR
-            }
-        };
-        println!("Switching to {:#?} mode", self.mode);
-        self.status_item.try_borrow_mut().unwrap().set_title(mode);
+        let new_mode = self.mode.toggle();
+        println!("Switching to {new_mode:#?} mode");
+        self.status_item
+            .try_borrow_mut()
+            .unwrap()
+            .set_image_with_system_symbol_name(
+                new_mode.sf_symbol(),
+                Some(new_mode.accessibility_description()),
+            );
+        self.mode = new_mode;
 
         self.configure_menu_items();
     }
 
     fn configure_menu_items(&self) {
+        let opposite_mode = self.mode.toggle();
         let menu_items = vec![
             MenuItem::toggle_mode(
-                match self.mode {
-                    Mode::Laptop => "Desktop Mode",
-                    Mode::Desktop => "Laptop Mode",
-                },
-                self.mode.sf_symbol(),
-                self.mode.accessibility_description(),
+                opposite_mode.description(),
+                opposite_mode.sf_symbol(),
+                opposite_mode.accessibility_description(),
                 self.weak_self.clone(),
             ),
             MenuItem::caffinate_item(self.caffinating, self.weak_self.clone()),

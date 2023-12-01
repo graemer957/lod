4c8d9f67fdcc9a5340f35666ce17dea5f13ae9a5
Graeme Read <graeme@sigma957.net>
2023-12-01 20:11:18 +0000

    Add default config.toml
    - Check config directory path exists, otherwise create
    - Check config file exists, otherwise use the example

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,10 @@
-use std::{error::Error, fs, fs::File, io::Write, path::PathBuf};
+use std::{
+    error::Error,
+    fs,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
 use tempfile::TempDir;
 use toml::Table;
 
@@ -11,6 +17,34 @@ pub struct Config {
 }
 
 impl Config {
+    fn check_exists_or_default() -> Result<PathBuf, Box<dyn Error>> {
+        let config_path = Path::new(env!("HOME")).join(".config/lod");
+        if matches!(Path::try_exists(&config_path), Ok(false) | Err(_)) {
+            fs::create_dir_all(&config_path)?;
+        }
+        if !config_path.is_dir() {
+            return Err(
+                "~/.config/lod should be a directory containing a `config.toml` file!".into(),
+            );
+        }
+
+        let config_file_path = config_path.join("config.toml");
+        if matches!(Path::try_exists(&config_file_path), Ok(false) | Err(_)) {
+            let default = include_str!("../example-config.toml");
+            let mut temp_file = File::create(&config_file_path)?;
+            write!(temp_file, "{default}")?;
+        }
+        if !config_file_path.is_file() {
+            return Err(
+                "~/.config/lod/config.toml should be a TOML file containing your \
+                configuration! Delete or rename this file and re-run for default to be created."
+                    .into(),
+            );
+        }
+
+        Ok(config_file_path)
+    }
+
     /// Attempt to load `Config` from storage
     ///
     /// # Errors
@@ -19,7 +53,8 @@ impl Config {
     /// - Unable to parse TOML
     /// - Creation of temp directory failed
     pub fn load() -> Result<Self, Box<dyn Error>> {
-        let toml = fs::read_to_string(format!("{}/.config/lod/config.toml", env!("HOME")))?;
+        let config_file_path = Self::check_exists_or_default()?;
+        let toml = fs::read_to_string(config_file_path)?;
         let toml = toml.parse::<Table>()?;
 
         let temp_dir = tempfile::tempdir()?;
@@ -54,9 +89,14 @@ impl Config {
     ) -> Result<PathBuf, Box<dyn Error>> {
         let applescript = toml
             .get(key)
-            .ok_or(format!("`{key}` is missing from config.toml. Please add."))?
+            .ok_or(format!(
+                "`{key}` is missing from config.toml. Please add, or revert to defaults."
+            ))?
             .as_str()
-            .ok_or(format!("`{key}` is malformed in config.toml. Please ensure it is valid AppleScript as a TOML string (see https://quickref.me/toml)."))?;
+            .ok_or(format!(
+                "`{key}` is malformed in config.toml. Please ensure it is valid \
+                AppleScript as a TOML string (see https://quickref.me/toml)."
+            ))?;
         let path = temp_dir.path().join(format!("{key}.scpt"));
         let mut temp_file = File::create(&path)?;
         write!(temp_file, "{applescript}")?;

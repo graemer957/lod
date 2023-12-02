681aac3bbcd1dcd8fa71515510cab3168732b996
Graeme Read <graeme@sigma957.net>
2023-12-02 09:25:39 +0000

    Update shared Rust material

--- ccov.sh
+++ ccov.sh
@@ -1,16 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
 set -e
 
-# shellcheck disable=SC2088
+SERVER=$1
+if [ -z "${SERVER}" ]; then
+	echo 'Need to specify server name! eg, `./ccov.sh apoc`'
+	exit 1;
+fi
+
 DEST_DIR="~/code/$(basename "$PWD")"
 
 # Remember to use '' to stop local shell expansion
-rsync -a --force --del --delete --delete-excluded -e ssh --exclude='*/.git' --exclude='target' . switch:"$DEST_DIR"
+rsync -a --force --del --delete --delete-excluded -e ssh --exclude='*/.git' --exclude='target' . $SERVER:"$DEST_DIR"
 
 # Execute tarpaulin remotely
-ssh -t switch "cd $DEST_DIR && cargo tarpaulin -o Html"
+ssh -t $SERVER ". .cargo/env && cd $DEST_DIR && cargo tarpaulin -o Html"
 
 # Copy back the report for inspection locally
-scp -q switch:"$DEST_DIR"/tarpaulin-report.html .
+scp -q $SERVER:"$DEST_DIR"/tarpaulin-report.html .
 
-# Last updated: 20231111
+# Last updated: 20231202

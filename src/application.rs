bfb96c76928a4f1a72cc090bab501bd177f1be06
Graeme Read <graeme@sigma957.net>
2025-10-18 20:37:16 +0100

    Switch out icrate for using objc2 crate directly

--- src/application.rs
+++ src/application.rs
@@ -1,13 +1,11 @@
 use std::{ffi::c_void, sync::mpsc::Receiver};
 
-use icrate::{
-    AppKit::{NSApplication, NSEvent, NSEventMaskAny},
-    Foundation::{NSDate, NSString},
-    objc2::{
-        ffi::{objc_autoreleasePoolPop, objc_autoreleasePoolPush},
-        rc::Id,
-    },
+use objc2::{
+    ffi::{objc_autoreleasePoolPop, objc_autoreleasePoolPush},
+    rc::Retained,
 };
+use objc2_app_kit::{NSApplication, NSEvent, NSEventMask};
+use objc2_foundation::{MainThreadMarker, NSDate, NSString};
 
 use crate::app_state::StateChangeMessage;
 
@@ -17,6 +15,9 @@ unsafe impl Send for AutoReleasePoolContext {}
 pub struct Application;
 
 impl Application {
+    /// # Panics
+    ///
+    /// If not called from main thread.
     pub fn run(
         receiver: &Receiver<StateChangeMessage>,
         mut callback: impl FnMut(StateChangeMessage),
@@ -28,19 +29,21 @@ impl Application {
         // 1. The author seems to be busy right now and not responding to my other raised PRs
         // 2. Tests do not work out of the box, so would have to fix those first
         unsafe {
+            let mtm = MainThreadMarker::new()
+                .expect("Application::run should be invoked from main thread!");
             let run_mode = NSString::from_str("kCFRunLoopDefaultMode");
             {
-                let app = NSApplication::sharedApplication();
+                let app = NSApplication::sharedApplication(mtm);
                 app.finishLaunching();
             }
 
             'event_loop: loop {
                 let pool_ctx = AutoReleasePoolContext(objc_autoreleasePoolPush());
-                let app = NSApplication::sharedApplication();
+                let app = NSApplication::sharedApplication(mtm);
 
-                let event: Option<Id<NSEvent>> = app
+                let event: Option<Retained<NSEvent>> = app
                     .nextEventMatchingMask_untilDate_inMode_dequeue(
-                        NSEventMaskAny,
+                        NSEventMask::Any,
                         Some(&NSDate::distantFuture()),
                         &run_mode,
                         true,
